e0725e119cefaed00b2c946aa2a030252334ab0a
Bazilio-san <github@bazilio.ru>
2025-09-01 09:35:07 +0300

    update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -162,5 +162,9 @@ module.exports = {
       },
     ],
     'vue/multi-word-component-names': 'off',
+    'no-restricted-syntax': 'off',
+    'no-await-in-loop': 'off',
+    'curly': ['error', 'all'],
+    'prefer-destructuring': ['error', { object: true, array: false }],
   },
 };
